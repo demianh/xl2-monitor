27b1854db063ba20e3acfe5d1511edbf6dc9fc27
Demian Holderegger <demian.holderegger@gmail.com>
2016-08-16 09:27:51 +0200

    package script documentation

--- app/package.sh
+++ app/package.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+echo ''
+echo '########################################################################'
+echo '####                                                                ####'
+echo '####    Make project artifacts before running this script!          ####'
+echo '####    IntelliJ -> Build -> Build Artifacts -> app:jar -> Build    ####'
+echo '####                                                                ####'
+echo '########################################################################'
+echo ''
+
 java -jar packr.jar packr-config.json
 
 zip -r -X ../xl2monitor.zip XL2Monitor.app
